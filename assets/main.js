4b3e70ef31d631142b4a9f48424d1f2ec5e654db
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-10 19:50:29 -0500

    Cancha futbol

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,4 @@
-alert("hola");
+// alert("hola");
+var botonFutbol = document.getElementById('botonFutbol');
+var botonBasquetbol = document.getElementById('botonBasquetbol');
+var botonTenis = document.getElementById('botonTenis');
