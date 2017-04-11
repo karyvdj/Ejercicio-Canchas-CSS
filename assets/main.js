dd97753f848a1cb0d1f35cfd04d2f6e4b16f99d5
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-11 11:48:21 -0500

    Canchas completas y botones funcionando

--- assets/main.js
+++ assets/main.js
@@ -7,6 +7,7 @@ var canchas = document.getElementsByClassName('canchas');
 
 botonFutbol.addEventListener("click",mostrarFutbol);
 botonBasquetbol.addEventListener("click",mostrarBasquetbol);
+botonTenis.addEventListener("click",mostrarTenis);
 
 function mostrarFutbol(){
   canchas[0].style.display = "block";
@@ -18,4 +19,10 @@ function mostrarBasquetbol(){
   canchas[1].style.display = "block";
   canchas[0].style.display = "none";
   canchas[2].style.display = "none";
+}
+
+function mostrarTenis() {
+  canchas[2].style.display = "block";
+  canchas[0].style.display = "none";
+  canchas[1].style.display = "none";
 }
