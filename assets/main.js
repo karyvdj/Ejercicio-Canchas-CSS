abe53b92b48fe3710ece5e20b582872e4c713c94
Kary V Dj <KaryVDj@iMac-de-Kary.local>
2017-04-10 22:20:10 -0500

    Cancha de tenis y comienzo de funcion

--- assets/main.js
+++ assets/main.js
@@ -2,3 +2,20 @@
 var botonFutbol = document.getElementById('botonFutbol');
 var botonBasquetbol = document.getElementById('botonBasquetbol');
 var botonTenis = document.getElementById('botonTenis');
+
+var canchas = document.getElementsByClassName('canchas');
+
+botonFutbol.addEventListener("click",mostrarFutbol);
+botonBasquetbol.addEventListener("click",mostrarBasquetbol);
+
+function mostrarFutbol(){
+  canchas[0].style.display = "block";
+  canchas[1].style.display = "none";
+  canchas[2].style.display = "none";
+}
+
+function mostrarBasquetbol(){
+  canchas[1].style.display = "block";
+  canchas[0].style.display = "none";
+  canchas[2].style.display = "none";
+}
